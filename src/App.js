274b52c15053e01a33db09e946048751c65967f9
Ben West <benjwest8@gmail.com>
2023-02-28 17:14:29 -0700

    routing and gh-pages not getting along

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ class App extends React.Component {
         <div className="App">
           {/* List Possible Page Routes Here */}
           <Routes>
-            <Route path="" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/ben-west" element={<Home />} />
             <Route path="/education" element={<Education />} />
             <Route path="/dev" element={<Dev />} />
             <Route path="/maps" element={<Maps />} />
